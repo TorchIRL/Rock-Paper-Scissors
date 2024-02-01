208b169da56f03db2e1d25390924c5e5e5e98499
TorchIRL <156404688+TorchIRL@users.noreply.github.com>
2024-02-01 16:19:35 +1000

    Rock Paper Scissors UI this added a ui to rock paper scissors v1 and implemented a scoring system

--- code.js
+++ code.js
@@ -1,35 +1,108 @@
+let computerScore = 0
+let playerScore = 0
 function getComputerChoice() {
     let choice = ['rock', 'paper', 'scissors']
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
+const buttons = document.querySelector('.content')
+
+const title = document.createElement('h1');
+title.textContent = 'Rock, Paper, Scissors';
+buttons.appendChild(title)
+
+const results = document.createElement('div');
+buttons.appendChild(results);
+
+const resultText = document.createElement('p');
+resultText.textContent = ''
+results.appendChild(resultText);
+
+const playersScore = document.createElement('p')
+playersScore.textContent = `Your Score: ${playerScore}`
+results.appendChild(playersScore)
+
+const computersScore = document.createElement('p')
+computersScore.textContent = `Computers Score: ${computerScore}`
+results.appendChild(computersScore)
+
+const sbtn = document.createElement('button');
+sbtn.setAttribute("id", 'scissors')
+sbtn.textContent = 'Scissors';
+buttons.appendChild(sbtn);
+
+const rbtn = document.createElement('button');
+rbtn.setAttribute("id", 'rock')
+rbtn.textContent = 'Rock';
+buttons.appendChild(rbtn);
+
+const pbtn = document.createElement('button');
+pbtn.setAttribute("id", 'paper')
+pbtn.textContent = 'Paper';
+buttons.appendChild(pbtn);
+
+let options = document.querySelector('.content')
+
+options.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case 'scissors':
+            console.log(playRound(playerSelection = 'scissors', computerSelection = getComputerChoice()))
+            break;
+        case 'rock':
+            console.log(playRound(playerSelection = 'rock', computerSelection = getComputerChoice()))
+            break;
+        case 'paper':
+            console.log(playRound(playerSelection = 'paper', computerSelection = getComputerChoice()))
+            break;
+    }
+})
+
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Your rock is wrapped up by paper'
+        resultText.textContent = 'You Lose! Your rock is wrapped up by paper'
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win! Your rock crushes scissors'
+        resultText.textContent = 'You Win! Your rock crushes scissors'
+        playerScore ++
+        playersScore.textContent = `Your Score: ${playerScore}`
     } else if (playerSelection === 'rock' && computerSelection === 'rock'){
-        return 'Tie!'
+        resultText.textContent = 'Tie!'
+        playerScore ++
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
+        playersScore.textContent = `Your Score: ${playerScore}`
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Win! Your paper wraps up rock'
+        resultText.textContent = 'You Win! Your paper wraps up rock'
+        playerScore ++
+        playersScore.textContent = `Your Score: ${playerScore}`
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'Tie!'
+        resultText.textContent = 'Tie!'
+        playerScore ++
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
+        playersScore.textContent = `Your Score: ${playerScore}`
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You Lose! Your paper is shredded by scissors'
+        resultText.textContent = 'You Lose! Your paper is shredded by scissors'
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        return 'You Win! Your scissors shred paper'
+        resultText.textContent = 'You Win! Your scissors shred paper'
+        playerScore ++
+        playersScore.textContent = `Your Score: ${playerScore}`
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You Lose! Your scissors are crushed by rock'
+        resultText.textContent = 'You Lose! Your scissors are crushed by rock'
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
     } else {
-        return 'Tie!'
+        resultText.textContent = 'Tie!'
+        playerScore ++
+        computerScore ++
+        computersScore.textContent = `Computers Score: ${computerScore}`
+        playersScore.textContent = `Your Score: ${playerScore}`
     }
 }
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = window.prompt('Choose Rock, Paper or Scissors.').toLowerCase()
-        let computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-}
-console.log(game())
